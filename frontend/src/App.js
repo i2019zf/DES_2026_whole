e6301f760540c5a464d89fbd8a3a0f563c83cfd6
i2019zf <msanthoshnagaraj@gmail.com>
2026-02-06 00:53:31 +0530

    updated permanant tunnel url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   // YOUR SPECIFIC CONFIGURATION
   const MY_GOOGLE_CLIENT_ID = "505819282429-hp3nhqnfun35rma9qlphh1818iek9meq.apps.googleusercontent.com";
-  const BASE_URL = "https://9fbf4dc12d8157.lhr.life";
+  const BASE_URL = "https://ff716cc4344c96.lhr.life";
 
   useEffect(() => {
     const script = document.createElement('script');
